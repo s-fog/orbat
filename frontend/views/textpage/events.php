98a37d2dbe667d19e3fb994d6fd27f7b891f60d0
Sergei Monakov <s-fog@yandex.ru>
2020-06-04 15:55:15 +0300

    update

--- frontend/views/textpage/events.php
+++ frontend/views/textpage/events.php
@@ -17,22 +17,27 @@ $this->params['name'] = $model->name;
             <?php foreach($events as $event) {
                 $filename = basename($event->image);
                 $filename = explode('.', $filename);
+                $blank = '';
+
                 if ($event->id == 3) {
                     $product = Product::findOne(3);
                     $url = Url::to(['catalog/view', 'alias1' => $product->category->alias, 'alias2' => $product->alias]);
                 } else if ($event->id == 4) {
                     $url = 'https://tatlin.ru/shop/interer_ot_pervogo_licza_orbat_2';
+                    $blank = ' target="_blank"';
                 } else {
                     $url = Url::to(['textpage/view', 'alias1' => Textpage::findOne(2)->alias, 'alias2' => $event->alias]);
                 }
+
+
                 ?>
                 <div class="news__item"
                      style="background-image: url(/images/thumbs/<?=$filename[0]?>-1200-400.<?=$filename[1]?>);">
                     <div class="news__itemInner">
                         <div class="news__itemDate"><?=Yii::$app->formatter->asDate($event->date, 'dd LLLL yyyy');?></div>
-                        <a href="<?=$url?>" class="news__itemName"><span><?=$event->name?></span></a>
+                        <a href="<?=$url?>" class="news__itemName"<?=$blank?>><span><?=$event->name?></span></a>
                         <div class="news__itemText"><?=$event->text?></div>
-                        <a href="<?=$url?>" class="news__itemButton">Подробнее</a>
+                        <a href="<?=$url?>" class="news__itemButton"<?=$blank?>>Подробнее</a>
                     </div>
                 </div>
             <?php } ?>
